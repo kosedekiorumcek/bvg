c8d9f9d950b9e9b9e9d088cd091220c4b3f92c28
kosedekiorumcek <kosedekiorumcek@gmail.com>
2026-09-16 11:56:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'bvg-radar-v2';
+// Sürüm v3: Tarayıcının eski index.html kopyasını zorla silip yenisini almasını sağlar
+const CACHE_NAME = 'bvg-radar-v3';
 
 self.addEventListener('install', (event) => {
-  // Yeni sürüm geldiğinde hemen devreye gir
-  self.skipWaiting();
+  self.skipWaiting(); // Yeni sürümü beklemeden anında devreye al
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         './',
         './index.html',
         './manifest.json'
-      ]).catch(err => console.log("Önbellek atlandı:", err));
+      ]).catch(err => console.log("Önbellek uyarısı:", err));
     })
   );
 });
@@ -18,6 +18,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
+        // v3 dışındaki tüm eski önbellekleri hafızadan sil
         keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
       );
     }).then(() => self.clients.claim())
@@ -25,9 +26,8 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // Önce internete sor (Network First), internet yoksa önbellekten aç
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }).catch(() => fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
